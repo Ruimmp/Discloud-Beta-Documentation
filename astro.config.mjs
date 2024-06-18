3a8aa3a84d20bff474fced290927711ea05c3dcc
Rui <61775818+Ruimmp@users.noreply.github.com>
2024-06-18 19:59:01 +0200

    Initial Commit

---
+++ astro.config.mjs
@@ -0,0 +1,38 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Discloud Docs",
+      defaultLocale: "pt",
+      locales: {
+        "en": {
+          label: "English",
+          locale: "en",
+        },
+        "pt": {
+          label: "Português",
+          locale: "pt",
+        },
+      },
+      social: {
+        github: "https://github.com/discloud",
+        twitter: "https://twitter.com/discloudbot",
+        discord: "https://discord.gg/rJBJqkHppW",
+        instagram: "https://www.instagram.com/discloudbot/",
+        youtube: "https://www.youtube.com/channel/UC9Q6b8Zb0Z1Jj5r6Z3Zvzjw",
+      },
+      logo: {
+        alt: "Discloud Logo",
+        src: "./src/assets/logo-no_background.png",
+      },
+      tableOfContents: {
+        minHeadingLevel: 2,
+        maxHeadingLevel: 3,
+      },
+      lastUpdated: true,
+    }),
+  ],
+});
